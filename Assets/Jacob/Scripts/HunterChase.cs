38e489c9cd412c34ec3fa257f067304a9d011588
Jacob M Miller <s179063@students.aie.edu.au>
2018-02-06 14:05:14 -0800

    Finished basic Hunter AI

---
+++ Assets/Jacob/Scripts/HunterChase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HunterChase: MonoBehaviour {
+
+    public GameObject chaseTarget;
+    NavMeshAgent agent;
+
+	void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        GameObject Hunter = GameObject.Find("Hunter");
+        HunterWander hunterWander = Hunter.GetComponent<HunterWander>();
+        chaseTarget = hunterWander.target;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        agent.destination = chaseTarget.transform.position;
+	}
+}
