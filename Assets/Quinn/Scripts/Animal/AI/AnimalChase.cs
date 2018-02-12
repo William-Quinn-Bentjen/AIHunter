b434a4ebc91269792cdf86043d90a82568af046e
Unknown <s179051@students.aie.edu.au>
2018-02-12 00:55:05 -0800

    attempted to fix errors with animals targets causeing errors

--- Assets/Quinn/Scripts/Animal/AI/AnimalChase.cs
+++ Assets/Quinn/Scripts/Animal/AI/AnimalChase.cs
@@ -19,36 +19,41 @@ public class AnimalChase : AnimalBehavior {
     private bool killedAnimal = false;
     public override void DoBehavior(AnimalBehaviorManager manager)
     {
-        //if (target == null)
-        //{
-        //    Debug.Log("NULL BITCH");
-        //}
-        //update distance to target and timer on how offten the animal can attack
-        UpdateDistance();
-        UpdateAttackSpeedTimer();
-        //is the animal in range and ready to attack?
-        if (distance <= AttackDistance && AttackSpeedTimer >= AttackSpeed)
+        GameObject test = target;
+        if (test != null)
         {
-            //deal damage if in range
-            //Debug.Log("AGGRESSIVE ANIMAL ATTACKED " + target.name + " FOR " + AttackDamage + " DAMAGE\nHUNTER HP NOW " + target.GetComponent<Health>().CurrentHP);
-            //did I just kill the thing I was chasing?
-            if (target.GetComponent<Health>().CurrentHP-AttackDamage <= 0)
+            //if (target == null)
+            //{
+            //    Debug.Log("NULL BITCH");
+            //}
+            //update distance to target and timer on how offten the animal can attack
+            UpdateDistance();
+            UpdateAttackSpeedTimer();
+            //is the animal in range and ready to attack?
+            if (distance <= AttackDistance && AttackSpeedTimer >= AttackSpeed)
             {
-                killedTarget = true;
-                if (target.tag == "Animal")
+                //deal damage if in range
+                //Debug.Log("AGGRESSIVE ANIMAL ATTACKED " + target.name + " FOR " + AttackDamage + " DAMAGE\nHUNTER HP NOW " + target.GetComponent<Health>().CurrentHP);
+                //did I just kill the thing I was chasing?
+                if (target.GetComponent<Health>().CurrentHP - AttackDamage <= 0)
                 {
-                    killedAnimal = true;
+                    killedTarget = true;
+                    if (target.tag == "Animal")
+                    {
+                        killedAnimal = true;
+                    }
                 }
+                target.GetComponent<Health>().IncrementHP(-AttackDamage);
             }
-            target.GetComponent<Health>().IncrementHP(-AttackDamage);
+            manager.agent.destination = target.transform.position + (target.GetComponent<Rigidbody>().velocity.normalized * ProjectedDistance);
+            Debug.DrawLine(transform.position, manager.agent.destination, Color.red);
         }
-        manager.agent.destination = target.transform.position + (target.GetComponent<Rigidbody>().velocity.normalized * ProjectedDistance);
-        Debug.DrawLine(transform.position, manager.agent.destination, Color.red);
     }
     public override bool CheckBehavior(AnimalBehaviorManager manager)
     {
+        GameObject test = target;
         //is the object past the stop distance and not in our vision?
-        if ((distance >= StopDistance && !manager.vision.visibleTargets.Contains(target.transform)) || killedTarget == true)
+        if (((distance >= StopDistance && !manager.vision.visibleTargets.Contains(target.transform)) || killedTarget == true) && test != null)
         {
             return true;
         }
@@ -65,11 +70,15 @@ public class AnimalChase : AnimalBehavior {
     }
     private void UpdateDistance()
     {
+        //used to avoid errors
         if (target == null)
         {
             distance = StopDistance;
         }
-        distance = Vector3.Distance(target.transform.position, gameObject.transform.position);
+        else
+        {
+            distance = Vector3.Distance(target.transform.position, gameObject.transform.position);
+        }
     }
     private void UpdateAttackSpeedTimer()
     {
